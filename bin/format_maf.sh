6029db705bb4b4bd9d0df85bcac4da4c372848aa
nikhil <nikhilkumar516@gmail.com>
2024-01-11 14:43:12 -0500

    Fix format script

--- bin/format_maf.sh
+++ bin/format_maf.sh
@@ -14,17 +14,15 @@ grep \
 
 awk \
     -F "\t" \
-    'NR==1
-    {
-        for(i=1;i<=NF;i++)
-        {
-            f[$i]=i
-        }
-        print "Hugo_Symbol\tEntrez_Gene_Id\tCenter\tTumor_Sample_Barcode\tFusion\tMethod\tFrame"
-    }
-    NR>1
-    {
-        print $(f["Hugo_Symbol"])"\t"$(f["Entrez_Gene_Id"])"\t"$(f["Center"])"\t"$(f["Tumor_Sample_Barcode"])"\t"$(f["Fusion"])"\t"$(f["Method"])"\t"$(f["Frame"])
+    'NR==1 { for(i=1;i<=NF;i++) \
+        { \
+            f[$i]=i \
+        } \
+        print "Hugo_Symbol\tEntrez_Gene_Id\tCenter\tTumor_Sample_Barcode\tFusion\tMethod\tFrame" \
+    } \
+    NR>1 \
+    { \
+        print $(f["Hugo_Symbol"])"\t"$(f["Entrez_Gene_Id"])"\t"$(f["Center"])"\t"$(f["Tumor_Sample_Barcode"])"\t"$(f["Fusion"])"\t"$(f["Method"])"\t"$(f["Frame"]) \
     }' \
     $1.grepped.txt \
     > \
@@ -34,24 +32,23 @@ awk \
 
 sed \
     '1s/$/\tDNA_support\tRNA_support/;2,$s/$/\tyes\tno/' \
-    $1.extracted.txt
+    $1.extracted.txt \
     > \
     $1.columns_added.txt
 
 ## Portal format output
 awk \
     -F "\t" \
-    'NR==1
-    {
-        for(i=1;i<=NF;i++)
-        {
-            f[$i]=i
-        }
-    }
-    {
-        print $(f["Hugo_Symbol"])"\t"$(f["Entrez_Gene_Id"])"\t"$(f["Center"])"\t"$(f["Tumor_Sample_Barcode"])"\t"$(f["Fusion"])"\t"$(f["DNA_support"])"\t"$(f["RNA_support"])"\t"$(f["Method"])"\t"$(f["Frame"])
+    'NR==1  \
+    { \
+        for(i=1;i<=NF;i++) \
+        { \
+            f[$i]=i \
+        } \
+    } \
+    { \
+        print $(f["Hugo_Symbol"])"\t"$(f["Entrez_Gene_Id"])"\t"$(f["Center"])"\t"$(f["Tumor_Sample_Barcode"])"\t"$(f["Fusion"])"\t"$(f["DNA_support"])"\t"$(f["RNA_support"])"\t"$(f["Method"])"\t"$(f["Frame"]) \
     }' \
+    $1.columns_added.txt \
     > \
     $1.portal.txt
-
-
